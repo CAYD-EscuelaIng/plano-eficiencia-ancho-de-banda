1b9e9cef8c55639ca9d63823ba9cf8460f4e9959
cayd-escuelaing <cayd.escuelaing@gmail.com>
2023-12-14 23:57:32 -0500

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plano-eficiencia-ancho-de-banda',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
